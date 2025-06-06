3e3e38dc9f7adb8a1566bc91c8e9acad0ac53ee0
Eduardo Rodriguez <eduardo_sdacr@hotmail.com>
2025-06-06 17:33:52 -0500

    feat: add frequent comments endpoint

--- src/comment-metrics/comment-metrics.controller.ts
+++ src/comment-metrics/comment-metrics.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { TotalCommentsDto } from './dto';
 import { CommentMetricsService } from './comment-metrics.service';
 import { JwtGuard } from '../auth/guard/jwt.guard';
+import { FrequentCommentDTO } from './dto/frequent-comments.dto';
 
+@UseGuards(JwtGuard)
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('comment-metrics')
 export class CommentMetricsController {
   constructor(private commentMetricsService: CommentMetricsService) {}
 
-  @UseGuards(JwtGuard)
   @Get('total-registered-comments')
   getTotalRegisteredComments(): Promise<TotalCommentsDto> {
     return this.commentMetricsService.getTotalRegisteredComments();
+  }
+
+  @Get('frequent-comments')
+  getFrequentComments(): Promise<FrequentCommentDTO[]> {
+    return this.commentMetricsService.getFrequentComments();
   }
 }
--- src/comment-metrics/comment-metrics.service.ts
+++ src/comment-metrics/comment-metrics.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { TotalCommentsDto } from './dto';
+import { FrequentCommentDTO } from './dto/frequent-comments.dto';
 
 @Injectable()
 export class CommentMetricsService {
@@ -10,5 +11,28 @@ export class CommentMetricsService {
     const total = await this.prisma.comments.count();
 
     return new TotalCommentsDto({ total });
+  }
+
+  async getFrequentComments(): Promise<FrequentCommentDTO[]> {
+    const comments = await this.prisma.comments.groupBy({
+      by: ['content'],
+      _count: {
+        content: true,
+      },
+      orderBy: {
+        _count: {
+          content: 'desc',
+        },
+      },
+      take: 100,
+    });
+
+    return comments.map(
+      ({ content, _count }) =>
+        new FrequentCommentDTO({
+          content,
+          count: _count.content,
+        }),
+    );
   }
 }
