1b222b00c67c5b67addb05b270c9accec67922bf
Eduardo Rodriguez <eduardo_sdacr@hotmail.com>
2025-06-08 18:01:13 -0500

    feat: add component frequent-comments table

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 import FeelingDistributionChart from "@/components/feeling-distribution-chart";
 import NumberCommentsChart from "../../components/number-comments-chart";
+import FrequentCommentsTable from "@/components/frequent-comments-table";
 
 function DashboardPage() {
   return (
-    <section className="h-[calc(100vh-7rem)] flex justify-center items-center">
-      <NumberCommentsChart />
-      <FeelingDistributionChart />
+    <section className="h-[calc(100vh-7rem)]">
+      <div className="flex justify-center items-center">
+        <NumberCommentsChart />
+        <FeelingDistributionChart />
+      </div>
+      <div className="flex justify-center items-center p-5">
+        <FrequentCommentsTable />
+      </div>
     </section>
   );
 }
